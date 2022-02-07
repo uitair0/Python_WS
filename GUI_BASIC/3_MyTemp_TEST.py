059694ceb1b2d9727bf7e337e53a72f1964f905b
uitair0 <uitair@naver.com>
2022-02-07 21:58:11 +0900

    21:02:07 test commit

--- GUI_BASIC/3_MyTemp_TEST.py
+++ GUI_BASIC/3_MyTemp_TEST.py
@@ -4,14 +4,22 @@ import tkinter
 root = Tk()
 root.title("Minsu Python Test")
 
-btn1 = Button(root, text = "button1")
+btn1 = Button(root, text="button1")
 btn1.pack()
-btn2 = Button(root, fg = "red", bg = "green", text = "button2")
+btn2 = Button(root, fg="red", bg="green", text="button2")
 btn2.pack()
 
 img1 = PhotoImage(file="icon.png")
 
 btn3 = Button(root, image=img1)
 btn3.pack()
+
+
+def clickEvent():
+    print("Button is clicked!!")
+
+
+btn4 = Button(root, command=clickEvent, text="Click!!")
+btn4.pack()
 
 root.mainloop()
