85ed77d1ca229aeb2fcc854d7efa105a8e9a6af8
uitair0 <99167697+uitair0@users.noreply.github.com>
2022-02-10 11:19:26 +0900

    Embedding Matplotlib to Tkinter

--- GUI_BASIC/8_combobox.py
+++ GUI_BASIC/8_combobox.py
@@ -3,9 +3,39 @@ from tkinter import *
 import tkinter
 import tkinter.ttk as ttk
 
+
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+
+
 root = Tk()
 root.title("Minsu Python Test")
 root.geometry("640x480")
+
+fig = Figure(figsize=(5, 4), dpi=100)
+# t = np.arange(0, 3, .01)
+# fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
+ax = fig.add_subplot(1,1,1)
+
+matplotlib.rcParams['font.family'] = 'AppleGothic'
+matplotlib.rcParams['font.size'] = 15
+matplotlib.rcParams['axes.unicode_minus'] = False
+
+values = [30, 25, 20, 13, 10, 2]
+labels = ['Python', 'Java', 'Javascript', 'C++', 'C#', 'ETC']
+plt.pie(values, labels=labels, autopct='%.1f%%',
+        startangle=90, counterclock=False)
+ax.pie(values, labels=labels, autopct='%.1f%%',
+        startangle=90, counterclock=False)
+
+canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+canvas.draw()
 
 values = [str(i) + "day" for i in range(1, 32)]  # 1 ~ 31
 combobox = ttk.Combobox(root, height=5, values=values)
@@ -25,5 +55,8 @@ def btncmd():
 
 btn = Button(root, text="Order", command=btncmd)
 btn.pack()
+
+canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
 
 root.mainloop()
